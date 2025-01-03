eb25192b67351f6e890d102c8e1c73f393ea9c01
Vishnu Bisht <vishnupratap24@gmail.com>
2025-01-03 18:47:42 +0530

    3rd commit

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
+		htmlFilePath := "script/index.html" 
+		htmlContent, err := os.ReadFile(htmlFilePath)
+		if err != nil {
+			http.Error(w, "Error loading HTML file", http.StatusInternalServerError)
+			fmt.Printf("Error reading HTML file: %s\n", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(htmlContent)
 	})
 
 	port := ":8080"
@@ -16,3 +26,4 @@ func main() {
 		fmt.Printf("Error starting server: %s\n", err)
 	}
 }
+
